fbcad19cdef862d4d3984ffac2b8e5f6efa46257
NaoTsutomu <iamdez96@gmail.com>
2023-11-20 21:16:16 +0800

    Print Hello world

--- GitHubTest/ViewController.swift
+++ GitHubTest/ViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let name = "John"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("Hello world!")
     }
 
 
